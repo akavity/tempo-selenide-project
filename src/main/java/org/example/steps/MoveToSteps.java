3fc630a4a1db694e6f4cb4e1aa10e719b07774bc
Basil Akavity <vakavity@gmail.com>
2023-06-23 13:36:10 +0300

    add MoveToPage, MoveToSteps

---
+++ src/main/java/org/example/steps/MoveToSteps.java
@@ -0,0 +1,22 @@
+package org.example.steps;
+
+import io.qameta.allure.Step;
+import lombok.extern.log4j.Log4j2;
+import org.example.pages.MoveToPage;
+
+@Log4j2
+public class MoveToSteps {
+    MoveToPage moveToPage = new MoveToPage();
+
+    @Step("Move to registration form")
+    public void moveToRegistrationForm() {
+        log.info("Move to registration form");
+        moveToPage.getRegistrationButton().click();
+    }
+
+    @Step("Move to pizza shopping")
+    public void moveToPizzaShopping() {
+        log.info("Move to  pizza shopping");
+        moveToPage.getPizzaButton().click();
+    }
+}
