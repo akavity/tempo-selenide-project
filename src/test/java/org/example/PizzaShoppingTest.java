0b865b6257297f66f5fa6580d2bc3377ba15cf0f
Basil Akavity <vakavity@gmail.com>
2023-07-26 13:17:07 +0300

    saved changes after update

--- src/test/java/org/example/PizzaShoppingTest.java
+++ src/test/java/org/example/PizzaShoppingTest.java
@@ -6,58 +6,55 @@ import org.example.models.PizzaTypeData;
 import org.example.steps.MoveToSteps;
 import org.example.steps.ShoppingSteps;
 import org.example.utils.JsonReader;
-import org.example.utils.Waiters;
+import org.example.utils.Utils;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.util.Collections;
 import java.util.List;
 
 public class PizzaShoppingTest extends BaseTest {
     ShoppingSteps pizzaShoppingSteps = new ShoppingSteps();
     MoveToSteps moveToSteps = new MoveToSteps();
-    Waiters waiters = new Waiters();
+    Utils utils = new Utils();
 
-    @Flaky
+
     @Test(description = "Check the price of 'Bavaria' pizza in the cart",
             dataProvider = "pizzaTestData", dataProviderClass = JsonReader.class)
     public void checkPriceOfGoodInBasketTopTest1(PizzaTestData pizzaData) {
         moveToSteps.moveToPizzaShopping();
-        pizzaShoppingSteps.isBasketEmpty();
         pizzaShoppingSteps.enterGoodButton(pizzaData.getBavariaPizzaName());
         pizzaShoppingSteps.enterSubmitButton();
 
         double actual = Double.parseDouble(pizzaShoppingSteps.getPriceFromBasketTop());
-        double expected = pizzaData.getBavariaPizzaPrice();
+        double expected = utils.roundOf(pizzaData.getBavariaPizzaPrice());
 
         Assert.assertEquals(actual, expected);
     }
 
-    @Flaky
+
     @Test(description = "Check the price of 'Tempting' pizza in the basket top",
             dataProvider = "pizzaTestData", dataProviderClass = JsonReader.class)
     public void checkPriceOfGoodInBasketTopTest2(PizzaTestData pizzaData) {
         moveToSteps.moveToPizzaShopping();
-        pizzaShoppingSteps.isBasketEmpty();
         pizzaShoppingSteps.enterGoodButton(pizzaData.getTemptingPizzaName());
         pizzaShoppingSteps.enterSubmitButton();
 
         double actual = Double.parseDouble(pizzaShoppingSteps.getPriceFromBasketTop());
-        double expected = pizzaData.getTemptingPizzaPrice();
+        double expected = utils.roundOf(pizzaData.getTemptingPizzaPrice());
 
         Assert.assertEquals(actual, expected);
     }
 
-    @Flaky
     @Test(description = "Check the price of 'Sicily' pizza in the basket top",
             dataProvider = "pizzaTestData", dataProviderClass = JsonReader.class)
     public void checkPriceOfGoodInBasketTopTest3(PizzaTestData pizzaData) {
         moveToSteps.moveToPizzaShopping();
-        pizzaShoppingSteps.isBasketEmpty();
         pizzaShoppingSteps.enterGoodButton(pizzaData.getSicilyPizzaName());
         pizzaShoppingSteps.enterSubmitButton();
 
         double actual = Double.parseDouble(pizzaShoppingSteps.getPriceFromBasketTop());
-        double expected = pizzaData.getSicilyPizzaPrice();
+        double expected = utils.roundOf(pizzaData.getSicilyPizzaPrice());
 
         Assert.assertEquals(actual, expected);
     }
@@ -66,35 +63,34 @@ public class PizzaShoppingTest extends BaseTest {
             dataProvider = "pizzaTestData", dataProviderClass = JsonReader.class)
     public void checkPriceOfTwoGoodsInBasketTop(PizzaTestData pizzaData) {
         moveToSteps.moveToPizzaShopping();
-        pizzaShoppingSteps.isBasketEmpty();
         pizzaShoppingSteps.enterGoodButton(pizzaData.getTemptingPizzaName());
         pizzaShoppingSteps.enterSubmitButton();
         pizzaShoppingSteps.enterGoodButton(pizzaData.getSicilyPizzaName());
         pizzaShoppingSteps.enterSubmitButton();
-        waiters.sleep();
+        utils.sleep();
 
         double actual = Double.parseDouble(pizzaShoppingSteps.getPriceFromBasketTop());
-        double expected = pizzaData.getTemptingPizzaPrice() + pizzaData.getSicilyPizzaPrice();
+        double expected = utils.roundOf(pizzaData.getTemptingPizzaPrice() + pizzaData.getSicilyPizzaPrice());
 
         Assert.assertEquals(actual, expected);
     }
 
+    @Flaky
     @Test(description = "Check the price of three pizzas in the basket top",
             dataProvider = "pizzaTestData", dataProviderClass = JsonReader.class)
     public void checkPriceOfTreeGoodsInBasketTop(PizzaTestData pizzaData) {
         moveToSteps.moveToPizzaShopping();
-        pizzaShoppingSteps.isBasketEmpty();
         pizzaShoppingSteps.enterGoodButton(pizzaData.getSicilyPizzaName());
         pizzaShoppingSteps.enterSubmitButton();
         pizzaShoppingSteps.enterGoodButton(pizzaData.getTemptingPizzaName());
         pizzaShoppingSteps.enterSubmitButton();
-        pizzaShoppingSteps.enterGoodButton(pizzaData.getBavariaPizzaName());
+        pizzaShoppingSteps.enterGoodButton(pizzaData.getVesuviusPizzaName());
         pizzaShoppingSteps.enterSubmitButton();
-        waiters.sleep();
+        utils.sleep();
 
         double actual = Double.parseDouble(pizzaShoppingSteps.getPriceFromBasketTop());
-        double expected = pizzaData.getTemptingPizzaPrice() + pizzaData.getSicilyPizzaPrice()
-                + pizzaData.getBavariaPizzaPrice();
+        double expected = utils.roundOf(pizzaData.getSicilyPizzaPrice() + pizzaData.getTemptingPizzaPrice()
+                + pizzaData.getVesuviusPizzaPrice());
 
         Assert.assertEquals(actual, expected);
     }
@@ -103,13 +99,12 @@ public class PizzaShoppingTest extends BaseTest {
             dataProvider = "pizzaTestData", dataProviderClass = JsonReader.class)
     public void checkPriceOfGoodInCart(PizzaTestData pizzaData) {
         moveToSteps.moveToPizzaShopping();
-        pizzaShoppingSteps.isBasketEmpty();
         pizzaShoppingSteps.enterGoodButton(pizzaData.getSicilyPizzaName());
         pizzaShoppingSteps.enterSubmitButton();
         moveToSteps.moveToBasket();
 
         double actual = Double.parseDouble(pizzaShoppingSteps.getPriceFromCart());
-        double expected = pizzaData.getSicilyPizzaPrice();
+        double expected = utils.roundOf(pizzaData.getSicilyPizzaPrice());
 
         Assert.assertEquals(actual, expected);
     }
@@ -118,7 +113,6 @@ public class PizzaShoppingTest extends BaseTest {
             dataProvider = "pizzaTestData", dataProviderClass = JsonReader.class)
     public void checkPriceOfTwoGoodsInCart(PizzaTestData pizzaData) {
         moveToSteps.moveToPizzaShopping();
-        pizzaShoppingSteps.isBasketEmpty();
         pizzaShoppingSteps.enterGoodButton(pizzaData.getTemptingPizzaName());
         pizzaShoppingSteps.enterSubmitButton();
         pizzaShoppingSteps.enterGoodButton(pizzaData.getSicilyPizzaName());
@@ -126,27 +120,27 @@ public class PizzaShoppingTest extends BaseTest {
         moveToSteps.moveToBasket();
 
         double actual = Double.parseDouble(pizzaShoppingSteps.getPriceFromCart());
-        double expected = pizzaData.getTemptingPizzaPrice() + pizzaData.getSicilyPizzaPrice();
+        double expected = utils.roundOf(pizzaData.getTemptingPizzaPrice() + pizzaData.getSicilyPizzaPrice());
 
         Assert.assertEquals(actual, expected);
     }
 
+    @Flaky
     @Test(description = "Check the price of three pizzas in the cart",
             dataProvider = "pizzaTestData", dataProviderClass = JsonReader.class)
     public void checkPriceOfTreeGoodsInCart(PizzaTestData pizzaData) {
         moveToSteps.moveToPizzaShopping();
-        pizzaShoppingSteps.isBasketEmpty();
-        pizzaShoppingSteps.enterGoodButton(pizzaData.getSicilyPizzaName());
-        pizzaShoppingSteps.enterSubmitButton();
         pizzaShoppingSteps.enterGoodButton(pizzaData.getTemptingPizzaName());
         pizzaShoppingSteps.enterSubmitButton();
-        pizzaShoppingSteps.enterGoodButton(pizzaData.getBavariaPizzaName());
+        pizzaShoppingSteps.enterGoodButton(pizzaData.getSicilyPizzaName());
+        pizzaShoppingSteps.enterSubmitButton();
+        pizzaShoppingSteps.enterGoodButton(pizzaData.getVesuviusPizzaName());
         pizzaShoppingSteps.enterSubmitButton();
         moveToSteps.moveToBasket();
 
         double actual = Double.parseDouble(pizzaShoppingSteps.getPriceFromCart());
-        double expected = pizzaData.getTemptingPizzaPrice() + pizzaData.getSicilyPizzaPrice()
-                + pizzaData.getBavariaPizzaPrice();
+        double expected = utils.roundOf(pizzaData.getTemptingPizzaPrice() + pizzaData.getSicilyPizzaPrice()
+                + pizzaData.getVesuviusPizzaPrice());
 
         Assert.assertEquals(actual, expected);
     }
@@ -155,12 +149,13 @@ public class PizzaShoppingTest extends BaseTest {
             dataProvider = "pizzaTypeData", dataProviderClass = JsonReader.class)
     public void checkSortingByType(PizzaTypeData pizzaData) {
         moveToSteps.moveToPizzaShopping();
-        pizzaShoppingSteps.isBasketEmpty();
         pizzaShoppingSteps.choosePizzaType(pizzaData.getPizzaType());
         pizzaShoppingSteps.enterResultButton();
 
         List<String> actual = pizzaShoppingSteps.getArrayPizzaNames();
         List<String> expected = pizzaData.getPizzaNames();
+        Collections.sort(actual);
+        Collections.sort(expected);
 
         Assert.assertEquals(actual, expected);
     }
