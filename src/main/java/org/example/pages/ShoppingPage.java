570872667102c4ea549fa4add928553fc65064b1
Basil Akavity <vakavity@gmail.com>
2024-02-05 12:26:07 +0300

    added new locators

--- src/main/java/org/example/pages/ShoppingPage.java
+++ src/main/java/org/example/pages/ShoppingPage.java
@@ -10,6 +10,7 @@ import static com.codeborne.selenide.Selenide.$$;
 public class ShoppingPage {
     private final SelenideElement submitButton =
             $(By.xpath("//div[@class='popupContent']//button"));
+    private final SelenideElement modifySubmitButton = $(By.xpath("//button[@id='modifyOrderBtn']"));
     private final SelenideElement resultButton = $(By.id("getResultPickup"));
     private final SelenideElement priceBasketTopField =
             $(By.xpath("//div[@class='basketOuter']//span[@class='bs-price']//span[@class='price_byn']"));
@@ -21,6 +22,16 @@ public class ShoppingPage {
     private final SelenideElement removeOrderButton = $(By.xpath("//a[@title='Удалить из заказа']"));
     private final SelenideElement openCloseButton =
             $(By.xpath("//div[@class='basket_placeholder']//div[@class='bs-toggler openClose']"));
+
+    public final SelenideElement addComponentButton(String component) {
+        return $(By.xpath("//div[@class='components']//label[contains(text(),'" + component + "')]" +
+                "/../..//div[contains(@class,'inc')]"));
+    }
+
+    public final SelenideElement removeComponentButton(String component) {
+        return $(By.xpath("//div[@class='components']//label[contains(text(),'" + component + "')]" +
+                "/../..//div[contains(@class,'dec')]"));
+    }
 
     public SelenideElement getProductNameField(String productName) {
         return $(By.xpath("//h3//span[contains(text(), '" + productName + "')]"));
@@ -36,6 +47,10 @@ public class ShoppingPage {
 
     public SelenideElement getSubmitButton() {
         return submitButton;
+    }
+
+    public SelenideElement getModifySubmitButton() {
+        return modifySubmitButton;
     }
 
     public SelenideElement getResultButton() {
